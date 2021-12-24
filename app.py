b729469c5fff2e57788b6ac96b7f075c4c86feb2
HooSL <leegntmd@naver.com>
2021-12-24 15:46:10 +0900

    add

--- app.py
+++ app.py
@@ -5,21 +5,19 @@ from seaborn.axisgrid import pairplot
 import matplotlib.pyplot as plt
 import seaborn as sns
 from PIL import Image
-from chart_app import run_chart_app, run_corr_app
-
-
+from chart_app import run_chart_app
 from gall_app import run_gall_app
 from mpg_ml_app import run_mpg_ml_app
 from price_ml_app import run_price_ml_app
-from chart_app import run_chart_app
+
 
 img = Image.open('data/car.jpg')
-st.set_page_config(page_title='중고 자동차 연비, 가격 예측',page_icon=img,
+st.set_page_config(page_title='Car MPG & Price',page_icon=img,
                     layout='wide',initial_sidebar_state='collapsed')
 
 def main():
     #제목
-    st.title('자동차 연비, 가격 예측')
+    st.title('Car MPG & Price')
 
     #사이드바
     menu = ['홈','데이터 검색','데이터 차트와 통계','연비 예측','가격 예측']
@@ -27,24 +25,33 @@ def main():
 
     #사이드바 설정
     if choice =='홈' :
-        st.write(' ')
-        st.write(' ')
         st.subheader('이 앱은 중고 자동차 연비와 가격에 대한 내용입니다.')
-        st.write(' ')
-        st.write(' ')
         st.write('1. 데이터 정보탭에서는 인공지능 학습에 사용된 데이터와 데이터의 통계를 보실 수 있습니다.')
-        st.write(' ')
-        st.write(' ')
         st.write('2. 검색 탭에서는 원하시는 차량의 데이터를 볼 수 있습니다.')
-        st.write(' ')
-        st.write(' ')
         st.write('3. 상관계수 차트탭에서는 데이터의 상관계수를 차트로 보실 수 있습니다.')
-        st.write(' ')
-        st.write(' ')
         st.write('4. 연비 예측탭에서는 차량의 데이터를 입력하면 해당차량의 연비를 예측드립니다.')
-        st.write(' ')
-        st.write(' ')
         st.write('5. 가격 예측탭에서는 차량의 데이터를 입력하면 해당차량의 가격를 예측해드립니다.')
+
+        st.markdown("## 환영합니다!  ✧⁺⸜(  •⌄•  )⸝⁺✧")
+        st.markdown("### Car MPG & Price에서는 고객님이 원하는 차량 데이터를 받고\n"
+         "### 머신러닝을 통해 그에 맞는 연비 또는 가격을 알려드립니다.")
+        st.subheader(' ')
+        st.subheader('메뉴 소개')
+        st.markdown("- 데이터 검색\n"
+                    "   - 머신러닝에 사용된 데이터\n"
+                    "   - 데이터 출처\n"
+                    "   - 차량 디자인 확인\n"
+                    "   - 현재 재고 확인\n")
+                
+        st.markdown("- 데이터 차트와 통계\n"
+                    "   - 데이터의 차트\n"
+                    "   - 데이터의 통계\n")
+                
+        st.markdown("- 연비 예측\n"
+                    "   - 차량 데이터 입력후 연비 예측\n")
+
+        st.markdown("- 가격 예측\n"
+                    "   - 차량 데이터 입력후 가격 예측\n")            
 
 
     elif choice == '데이터 검색':
@@ -57,20 +64,9 @@ def main():
     elif choice == '연비 예측':
         run_mpg_ml_app()
 
+
     elif choice == '가격 예측':
         run_price_ml_app()
-
-
-
-
-
-
-# 검색 기능 , 최대 최소값
-
-
-
-
-
 
 
 
--- chart_app.py
+++ chart_app.py
@@ -9,13 +9,13 @@ from PIL import Image
 
 def run_chart_app():
     #서브헤더
-    st.subheader('상관계수와 상관계수의 차트 입니다.')
+    st.subheader('데이터 컬럼별 차트 입니다.')
     #1. csv파일 가져오기
     df = pd.read_csv('data/Car.csv',index_col=0)
     
     df.columns=['brand','model','model year','mileage','enginsize','transmission','fuel','mpg','price']
 
-    st.subheader('상관계수')
+    st.subheader('차트')
     
     word = st.text_input('차량 제조사를 입력하세요')
     word.lower()
@@ -36,6 +36,14 @@ def run_chart_app():
     st.subheader('데이터 컬럼별 통계치')
     st.dataframe(df_search.describe())
 
+    st.subheader('데이터의 최소값과 최대값')
+    number_colums = df.columns[df.dtypes != object]
+    selected_minmax_columes = st.selectbox('최소,최대값 컬럼 선택',number_colums)
 
+    #5-2. 선택한 컬럼의 최소값에 해당되는 사람의 데이터 출력
+    min_data = df.loc[df[selected_minmax_columes] == df[selected_minmax_columes].min(),]
+    st.dataframe(min_data)
 
-
+    #5-3. 선택한 컬럼의 최대값에 해당되는 사람의 데이터 출력
+    max_data = df.loc[df[selected_minmax_columes] == df[selected_minmax_columes].max(),]
+    st.dataframe(max_data)
